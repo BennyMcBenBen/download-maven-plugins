0511a26d9971f25961544433d612e2b5ddd8384e
Ben Zoller <benjamin.zoller@gmail.com>
2013-09-11 20:47:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample project which when built with Maven will download plugins that are needed.
---
+++ download-maven-plugins-jar/src/test/java/com/github/bennymcbenben/AppIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.github.bennymcbenben;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Unit test for simple App.
+ */
+ @Category(IntegrationTest.class)
+public class AppIntegrationTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
